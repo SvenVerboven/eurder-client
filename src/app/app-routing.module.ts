6e393d4bd50dab4242932a462ebd100b19c3244d
Sven Verboven <sven_verboven@hotmail.com>
2020-05-04 17:21:29 +0200

    added dropdown in toolbar for navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import {Routes, RouterModule} from '@angular/router';
 import {ItemsComponent} from './items/items.component';
 import {UsersComponent} from './users/users.component';
 import {OrdersComponent} from './orders/orders.component';
+import {CreateItemComponent} from './create-item/create-item.component';
 
 const routes: Routes = [
   {path: 'items', component: ItemsComponent},
   {path: 'users', component: UsersComponent},
   {path: 'orders', component: OrdersComponent},
+  {path: 'create-item', component: CreateItemComponent},
   {path: '', redirectTo: '/items', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ItemsComponent } from './items/items.component';
-import { OrdersComponent } from './orders/orders.component';
-import { UsersComponent } from './users/users.component';
-import { AppRoutingModule } from './app-routing.module';
-import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
+import {AppComponent} from './app.component';
+import {TopBarComponent} from './top-bar/top-bar.component';
+import {ItemsComponent} from './items/items.component';
+import {OrdersComponent} from './orders/orders.component';
+import {UsersComponent} from './users/users.component';
+import {AppRoutingModule} from './app-routing.module';
+import {BottomBarComponent} from './bottom-bar/bottom-bar.component';
 import {Ng2SearchPipeModule} from 'ng2-search-filter';
 import {FormsModule} from '@angular/forms';
-import { FilterByItemNamePipe } from './filter-by-item-name.pipe';
+import {FilterByItemNamePipe} from './filter-by-item-name.pipe';
+import {CreateItemComponent} from './create-item/create-item.component';
 
 @NgModule({
   declarations: [
@@ -21,16 +22,19 @@ import { FilterByItemNamePipe } from './filter-by-item-name.pipe';
     OrdersComponent,
     UsersComponent,
     BottomBarComponent,
-    FilterByItemNamePipe
+    FilterByItemNamePipe,
+    CreateItemComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     Ng2SearchPipeModule,
-    FormsModule
+    FormsModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
