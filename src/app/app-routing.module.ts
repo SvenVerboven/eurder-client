281bfc1c04668f30cae40e5dc92204abdbfadd45
Sven Verboven <sven.verboven@colruytgroup.com>
2022-07-12 19:54:43 +0200

    Feature survey.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'survey', loadChildren: () => import('./survey/survey.module').then((m) => m.SurveyModule)},
   {path: '', redirectTo: 'survey', pathMatch: 'full'},
+  {path: 'survey', loadChildren: () => import('./survey/survey.module').then((m) => m.SurveyModule)},
 ];
 
 @NgModule({
