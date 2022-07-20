c6129e882a737f82e0b5cf538ac695b6b27083e4
Sven Verboven <sven.verboven@colruytgroup.com>
2022-07-20 17:51:25 +0200

    Feature: survey.

---
+++ src/app/survey/services/survey-http.service.ts
@@ -0,0 +1,18 @@
+import { Observable } from 'rxjs';
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../../../environments/environment.prod';
+import { Survey } from '../models/survey.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SurveyHttpService {
+  constructor(private readonly http: HttpClient) {}
+
+  save(survey: Survey): Observable<Survey> {
+    return this.http.post<Survey>(`${environment.url.baseUrl}/surveys`, survey);
+  }
+}
