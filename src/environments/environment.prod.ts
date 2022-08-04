48945479bd0ea84f31532221bf0bdc1acf77fdaf
Sven Verboven <sven.verboven@colruytgroup.com>
2022-08-04 14:54:05 +0200

    Changed backend url to PROD.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   url: {
     // TODO replace url
-    baseUrl: 'http://localhost:8080',
-    // baseUrl: 'https://order-sven-verboven.herokuapp.com',
+    // baseUrl: 'http://localhost:8080',
+    baseUrl: 'https://order-sven-verboven.herokuapp.com',
   },
 };
